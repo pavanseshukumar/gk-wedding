082a7833aaf60c64af7163d120b23fba757f6f55
pavan31 <poluparthipavanseshukumar@gmail.com>
2025-11-11 20:46:36 +0530

    resolved build issue

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import { Flower2, CalendarDays, ArrowRight, Timer } from "lucide-react";
 import Image from "next/image";
 
-import mainImage from "@/public/images/1.jpg";
+import mainImage from "@/public/images/1.JPG";
 
 import { Button } from "@/components/ui/button";
 
